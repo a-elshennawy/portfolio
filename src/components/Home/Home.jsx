a902b106dfeee0f91a233c80cdd359e9e271692a
shennawy <ahmedshennawy997@gmail.com>
2025-06-06 03:13:03 +0300

    no footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,13 @@
 import Header from "./Header/Header";
 import Samples from "./Samples/Samples";
 import UpBtn from "../UpBtn/UpBtn";
-import Footer from "./Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Samples />
-      <Footer />
+
       <UpBtn />
     </>
   );
