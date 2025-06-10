52efce1e2da287a1face51afe60110e30b35dafb
shennawy <ahmedshennawy997@gmail.com>
2025-06-10 19:19:50 +0300

    sample app card styled / webp

--- src/components/Home/Samples/Samples.jsx
+++ src/components/Home/Samples/Samples.jsx
@@ -15,15 +15,10 @@ export default function Samples() {
               </div>
               <div className="appCard col-lg-3 col-12 row">
                 <div className="img col-12">
-                  <img src="/images/coffeeShop.jpeg" alt="" loading="lazy" />
+                  <img src="/images/coffeeShop.webp" alt="" loading="lazy" />
                 </div>
                 <div className="details col-12">
-                  <p>
-                    a react app for a coffe shop as my graduation project using
-                    local storage for log in and sign up and log out also to
-                    save orders and reservations getting drinks and posts
-                    through a local JSON files also fully responsive
-                  </p>
+                  <h3>butter coffee-shop</h3>
                   <button className="goBtn">
                     <Link
                       to={"https://butter-react.netlify.app/"}
@@ -41,16 +36,10 @@ export default function Samples() {
               </div>
               <div className="appCard col-lg-3 col-12 row">
                 <div className="img col-12">
-                  <img src="/images/watchesShop.jpeg" alt="" loading="lazy" />
+                  <img src="/images/watchesShop.webp" alt="" loading="lazy" />
                 </div>
                 <div className="details col-12">
-                  <p>
-                    a react vite web app for a watches store in both arabic and
-                    english and a protected CRUD system route connected to
-                    database using firebase and OAuth authentication for admin
-                    access and form submitting for orders and email notification
-                    for each order made using Gmail API V1
-                  </p>
+                  <h3>elsokarya for watches</h3>
                   <button className="goBtn">
                     <Link to={"https://elsokarya.net/"} target="_blank">
                       live webApp
@@ -65,14 +54,10 @@ export default function Samples() {
               </div>
               <div className="appCard col-lg-3 col-12 row">
                 <div className="img col-12">
-                  <img src="/images/dayapp.jpeg" alt="" loading="lazy" />
+                  <img src="/images/dayapp.webp" alt="" loading="lazy" />
                 </div>
                 <div className="details col-12">
-                  <p>
-                    basic HTML / CSS / JS a daily app getting live weather cast,
-                    prayer times based on location, also additional quran app
-                    and azkar also fully responsive across all screens
-                  </p>
+                  <h3>daily app</h3>
                   <button className="goBtn">
                     <Link
                       to={"https://dailyguide.netlify.app/"}
@@ -85,13 +70,10 @@ export default function Samples() {
               </div>
               <div className="appCard col-lg-3 col-12 row">
                 <div className="img col-12">
-                  <img src="/images/gym.jpeg" alt="" loading="lazy" />
+                  <img src="/images/gym.webp" alt="" loading="lazy" />
                 </div>
                 <div className="details col-12">
-                  <p>
-                    a basic and static gym webApp design i used basic HTML / CSS
-                    wich is also fully responsive with some animations
-                  </p>
+                  <h3>gladiator gym</h3>
                   <button className="goBtn">
                     <Link
                       to={"https://gladiator-gym-psi.vercel.app/"}
@@ -109,14 +91,10 @@ export default function Samples() {
               </div>
               <div className="appCard col-lg-3 col-12 row">
                 <div className="img col-12">
-                  <img src="/images/sshop.jpeg" alt="" loading="lazy" />
+                  <img src="/images/sshop.webp" alt="" loading="lazy" />
                 </div>
                 <div className="details col-12">
-                  <p>
-                    basic HTML / CSS / JS e-commerce website using local storage
-                    for for log in and sign up and log out also to save orders
-                    and checkout, using APIs to fetch random projects
-                  </p>
+                  <h3>SShop</h3>
                   <button className="goBtn">
                     <Link to={"https://e-sshop.netlify.app/"} target="_blank">
                       check demo
@@ -136,14 +114,10 @@ export default function Samples() {
               </div>
               <div className="appCard col-lg-3 col-12 row">
                 <div className="img col-12">
-                  <img src="/images/hurst.jpeg" alt="" loading="lazy" />
+                  <img src="/images/hurst.webp" alt="" loading="lazy" />
                 </div>
                 <div className="details col-12">
-                  <p>
-                    a landing page for a furniture shop using basic HTML / CSS
-                    and bootstrap with some additional animations also fully
-                    responsive
-                  </p>
+                  <h3>hurst furniture</h3>
                   <button className="goBtn">
                     <Link to={"https://hurstf.netlify.app/"} target="_blank">
                       check demo
@@ -158,14 +132,10 @@ export default function Samples() {
               </div>
               <div className="appCard col-lg-3 col-12 row">
                 <div className="img col-12">
-                  <img src="/images/confer.jpeg" alt="" loading="lazy" />
+                  <img src="/images/confer.webp" alt="" loading="lazy" />
                 </div>
                 <div className="details col-12">
-                  <p>
-                    a landing page for a conferances event using basic HTML /
-                    CSS and bootstrap with some additional animations also fully
-                    responsive
-                  </p>
+                  <h3>confer</h3>
                   <button className="goBtn">
                     <Link
                       to={"https://confertest.netlify.app/"}
@@ -183,14 +153,10 @@ export default function Samples() {
               </div>
               <div className="appCard col-lg-3 col-12 row">
                 <div className="img col-12">
-                  <img src="/images/fixera.jpeg" alt="" loading="lazy" />
+                  <img src="/images/fixera.webp" alt="" loading="lazy" />
                 </div>
                 <div className="details col-12">
-                  <p>
-                    a landing page for a tech shop using basic HTML / CSS and
-                    bootstrap with some additional animations also fully
-                    responsive
-                  </p>
+                  <h3>fixera tech-shop</h3>
                   <button className="goBtn">
                     <Link to={"https://fixera.netlify.app/"} target="_blank">
                       check demo
