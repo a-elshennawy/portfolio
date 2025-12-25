e455930d91fc0b1782a1f38cd71fa7398e58897c
shennawy <ahmedshennawy997@gmail.com>
2025-12-25 13:42:40 +0200

    translation

--- app/[locale]/light-mode/page.jsx
+++ app/[locale]/light-mode/page.jsx
@@ -3,16 +3,18 @@ import { Link } from "@/i18n/routing";
 import "./LightMode.css";
 import Image from "next/image";
 import useMobile from "@/hooks/useMobile";
+import { useTranslations } from "next-intl";
 
 function LightMode() {
+  const t = useTranslations("texts");
   const { isMobile } = useMobile();
   return (
     <>
       <section className="z-2 p-0 lightModeComp">
         <div className="content text-center">
-          <h1>TURN OFF THAT LIGHT</h1>
+          <h1>{t("lightMode_text")}</h1>
           <Link className="my-5 mx-auto p-2 toHomeBtn" href="/">
-            turn it off
+            {t("lightMode_btn")}
           </Link>
           <Image
             src="/avatar/lightMode.webp"
