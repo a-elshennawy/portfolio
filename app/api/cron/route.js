88e28a08f20574a914ddfb09518704ab234bd460
shennawy <ahmedshennawy997@gmail.com>
2026-01-25 12:10:17 +0200

    vercel

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,20 +1,29 @@
+export const dynamic = "force-dynamic";
+
 import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
-  );
+  try {
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.SUPABASE_SERVICE_ROLE_KEY,
+    );
 
-  const { data, error } = await supabase
-    .from("testimonials")
-    .select("id")
-    .limit(1);
+    const { data, error } = await supabase
+      .from("testimonials")
+      .select("id")
+      .limit(1);
 
-  if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json({
+      success: true,
+      message: "Supabase pinged successfully",
+    });
+  } catch (err) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
   }
-
-  return NextResponse.json({ success: true });
 }
